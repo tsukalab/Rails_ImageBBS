7f7646b666d7105923fc6840056f327431a3798d
hiriorima <g2117001@fun.ac.jp>
2017-05-29 00:40:56 +0900

    Add image uploader

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -69,6 +69,6 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:name, :description)
+      params.require(:project).permit(:name, :description, :image, :image_cache, :remove_image)
     end
 end
